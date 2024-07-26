cbb664fc2788b63ea5691a0b72831e324a345743
webster-rafael <webster.rafael2014@gmail.com>
2024-07-26 13:47:00 -0400

    novo

--- src/components/Oferta/cards.tsx
+++ src/components/Oferta/cards.tsx
@@ -9,7 +9,7 @@ interface Props {
 const CardsLancamento = ({ img, title, price, slug }: Props) => {
   return (
     <Link to={`/produtos/${slug}`} className="border h-76 px-1 w-full py-2 rounded-sm shadow-md hover:scale-105 hover:border-purple-300">
-      <img className="w-full h-56" src={img} alt="" />
+      <img className="w-full h-52" src={img} alt="" />
       <p className="text-sm overflow-hidden truncate pt-3">{title}</p>
       <div className="flex items-baseline gap-1 w-full pb-1">
         <span className="font-semibold text-zinc-950 text-sm">
